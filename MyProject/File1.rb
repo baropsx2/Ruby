ae3ac602e8d37284ceda853e4cade78e5228cd06
baropsx2 <baropsx2@gmail.com>
2019-04-04 17:04:51 -0600

    Messagee

--- MyProject/File1.rb
+++ MyProject/File1.rb
@@ -3,7 +3,7 @@ class A
     # This is the first type of the method
 
     def display_data()
-        puts "This is the first method without arguments"
+        puts "This is the first method without arguments"  
         a=100
         b=200
         c=a+b
